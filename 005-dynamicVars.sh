5e104307b31114c9c6a583ecec06ae21372b066b
CodingManoj <mvp2612@hotmail.com>
2024-03-10 21:41:48 -0400

    Bash Auto Push

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -5,7 +5,14 @@
 
 DATE="$(date +%F)"
 NO_OF_SESSIONS="$(who|wc -l)"
+TEAM=$1 
 echo -e "Todays date is ${DATE}" 
 echo -e "Total Opened Sessions Are $NO_OF_SESSIONS"
 
 echo "Script Name is : $0"
+
+echo "Name of the team running the script is :  $TEAM"
+
+### bash scriptName.sh    arg1   arg2   arg3   arg4 . . . . . . arg9
+### bash scriptName.sh  firstArg   secondArg  thirdArd 
+### bash abc.sh  10   20   30 

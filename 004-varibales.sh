73f80df1cf787c4c1a322789959b9a2bef750423
CodingManoj <mvp2612@hotmail.com>
2024-03-10 21:21:11 -0400

    Bash Auto Push

--- 004-varibales.sh
+++ 004-varibales.sh
@@ -8,7 +8,8 @@
 # Declaring the variables
 
 a=10
-b=def 
+b=def
+CUSTDATA="shipping"
 
 # In bash, everything will be considered as string by default.
 
@@ -17,12 +18,14 @@ b=def
 # $ is a special variableand this helps you in printing the value of the variable 
 echo $a
 echo "The value of the variable b is $b"
-echo "The value of the variable a is ${b}"
+echo -e "The value of the variable a is \e[31m ${a} \e[0m"
 
 # Since in bash and shell, there are lot of versions, both of the syntaxes are valid, in case if anyone doesn't work, prefer to use the other one.
 # echo $a 
 # echo ${a}
 
-# Printing a variable, wher ethe value of it is not declared , what will happen ?
-
+# Printing a variable, wher ethe value of it is not declared , what will happen ? It will print null.
 echo "The value of the variable d is ${d}"
+
+# rm -rf /data/prod/${CUSTDATA} ====> /data/prod/shipping
+# rm -rf /data/prod/${APPDATA} ====> /data/prod

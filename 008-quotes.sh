053554e1ce6869c116d50cb2cdf5bd9e3d6adbd7
CodingManoj <mvp2612@hotmail.com>
2024-03-10 21:55:15 -0400

    Bash Auto Push

---
+++ 008-quotes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ""   : Double Quotes 
+# ''   : Single Quotes   : Single Quotes When used special variables will make special variable to lose it's power.
+
+a=100 
+echo $?
+echo '$?'
+echo "Value of is $?" 
+echo 'Value of is $?' 

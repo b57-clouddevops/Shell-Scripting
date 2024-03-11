97b89ce8e5b3075c82d1e2edc78dc23900e56dfe
CodingManoj <mvp2612@hotmail.com>
2024-03-10 22:05:36 -0400

    Bash Auto Push

--- 009-read.sh
+++ 009-read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 
 read -p "Enter Your name:" name
-echo "Name of the person running the script is $name" 
+echo "Name of the person running the script is $name"
---
+++ 010-functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# There are 4 types of commands available : 
+
+# 1) Binary 
+# 2) Aliases 
+# 3) Shell Built-In Commands 
+# 4) Functions

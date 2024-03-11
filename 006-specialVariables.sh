cc8961f5e196aa5788eadeb8eb3a09612bc4c62d
CodingManoj <mvp2612@hotmail.com>
2024-03-10 21:36:12 -0400

    Bash Auto Push

---
+++ 006-specialVariables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# Special Variables are the pre-defined variables with specific special pre-defined powers.
+
+# Special Variables are $0 to $9, $* , $@, $# ,  $$
+
+# Can you create special variables on Bash ? 100% No , you can just use the special variables that are offered.
+
+a=3000 
+
+echo "Value of a is $a" 
+echo "Script Name is : $0"
+
+
+
+
+
+$0 : Prints the name of the script you're running

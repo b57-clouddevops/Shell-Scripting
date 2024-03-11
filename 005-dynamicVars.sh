ea13143f93c02a30ed016abc27583c5ce3c56d9b
CodingManoj <mvp2612@hotmail.com>
2024-03-10 21:31:04 -0400

    Bash Auto Push

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DATE="11/MARCH/2024"
-NO_OF_SESSIONS="4"
+# DATE="11/MARCH/2024"
+# NO_OF_SESSIONS="4"       this is called as hardCoding and hardCoding is a bad way and whereever possible you should fetch the values dynamically.
+
+DATE="$(date +%F)"
+NO_OF_SESSIONS="$(who|wc -l)"
 echo -e "Todays date is ${DATE}" 
 echo -e "Total Opened Sessions Are $NO_OF_SESSIONS"

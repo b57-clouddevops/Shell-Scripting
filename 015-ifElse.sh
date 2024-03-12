6934e06fcfc82d417ed3ab3ea00fa94e48806c47
CodingManoj <mvp2612@hotmail.com>
2024-03-11 21:40:37 -0400

    Bash Auto Push

--- 015-ifElse.sh
+++ 015-ifElse.sh
@@ -5,7 +5,17 @@ echo -e "Demo on if, if else, else if conditions Usage"
 ACTION=$1
 
 if [ "$ACTION" = "start" ] ; then 
-    echo -e "\e[32m Starting Dispatch Service \e[0m" 
+    echo -e "\e[32m Starting Dispatch Service \e[0m"
+    exit 0 
+
+elif [ "$ACTION" = "stop" ] ; then 
+    echo -e "\e[31m Stopping Dispatch Service \e[0m" 
+    exit 1
+
+elif [ "$ACTION" = "restart" ] ; then 
+    echo -e "\e[34m Restarting Dispatch Service \e[0m" 
+    exit 2
 else 
-    echo -e "\e[35m Valid options is start only \e[0m"
+    echo -e "\e[35m Valid options are start stop restart only \e[0m"
+    exit 3
 fi

5a93f0e6a831b0b4259a25d6658f2cafa2fd1ee7
CodingManoj <mvp2612@hotmail.com>
2024-03-11 21:29:46 -0400

    Bash Auto Push

--- 013-conditions.sh
+++ 013-conditions.sh
@@ -15,14 +15,18 @@ ACTION=$1
 case $ACTION in 
     start)
         echo -e "\e[32m Starting Dispatch Service \e[0m" 
+        exit 0
         ;;
     stop)
         echo -e "\e[31m Stopping Dispatch Service \e[0m" 
+        exit 1
         ;;
     restart)
         echo -e "\e[33m Restarting Dispatch Service \e[0m" 
+        exit 2
         ;;
     *)
         echo -e "\e[35m Valid options are: start  stop  restart   only \e[0m"
         echo -e "\e[33m Usage: \e[0m \n\tbash conditions.sh  start"
+        exit 3
 esac 

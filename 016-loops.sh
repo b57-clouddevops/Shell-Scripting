e05bec582d5c8b31851c699fecefc24feaa66b1c
CodingManoj <mvp2612@hotmail.com>
2024-03-11 21:46:10 -0400

    Bash Auto Push

--- 016-loops.sh
+++ 016-loops.sh
@@ -6,8 +6,16 @@
 #     1) For Loop        ( When you known how many times to be executed, we use for loop )
 #     2) While Loop      ( when you don't know how many time to be executed, we use this and while only runs if the condition is trye )
 
-echo value is 10
-echo value is 20
-echo value is 30
-echo value is 40
-echo value is 50
+# echo value is 10
+# echo value is 20
+# echo value is 30
+# echo value is 40
+# echo value is 50
+
+# for x in a b c d e ; do 
+#     echo print $x
+# done 
+
+for x in 10 20 30 40 50 ; do  
+    echo -e "Vales are $x"
+done 

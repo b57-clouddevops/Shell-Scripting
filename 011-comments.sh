666eea256bd06ad1d0d240def8c21c1ecc2e584f
CodingManoj <mvp2612@hotmail.com>
2024-03-11 20:59:19 -0400

    Bash Auto Push

---
+++ 011-comments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a single line comment
+
+# Here is an example of multiline comment 
+
+<<COMMENT
+echo "Cloud DevOps Training"
+echo "Shall Scripting"
+a=100
+b=300
+echo $a
+COMMENT 
+echo "Value of b is $b"

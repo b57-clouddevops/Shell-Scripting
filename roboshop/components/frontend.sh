8b7edea7b4c6851817589d6c99b87b3347c82c3b
CodingManoj <mvp2612@hotmail.com>
2024-03-12 20:58:50 -0400

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,7 +4,7 @@
 # Check whether you're running this script with sudo or a root user or not.If not, exit the script
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
-    echo -e "\e[31m This script is expected to to run with sudo\e[0m   \n\t Ex:  bash scriptName compMame"
+    echo -e "\e[31m This script is expected to run with sudo or as a root user \e[0m   \n\t Ex:  bash scriptName compMame"
     exit 1
 fi 
 

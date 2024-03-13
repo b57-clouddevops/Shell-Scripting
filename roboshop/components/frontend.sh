248e8407ca994983c54aa796270c91d0ffce6eff
CodingManoj <mvp2612@hotmail.com>
2024-03-12 21:02:47 -0400

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,9 +10,24 @@ fi
 
 echo "Intalling Nginx Web Server"
 dnf install nginx -y        &>>  /tmp/frontend.log
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo "\e[31m Failure \e[0m"
+fi 
 
 echo "Enabling the service"
 systemctl enable nginx      &>>  /tmp/frontend.log
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo "\e[31m Failure \e[0m"
+fi 
 
 echo "Starting the Web Server"
 systemctl start nginx       &>>  /tmp/frontend.log
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo "\e[31m Failure \e[0m"
+fi 

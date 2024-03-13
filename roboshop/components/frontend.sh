c9c07eca7396a1ba71e71205f8955574949f0b10
CodingManoj <mvp2612@hotmail.com>
2024-03-12 21:00:49 -0400

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,15 +4,15 @@
 # Check whether you're running this script with sudo or a root user or not.If not, exit the script
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
-    echo -e "\e[31m This script is expected to run with sudo or as a root user \e[0m   \n\t Ex:  bash scriptName compMame"
+    echo -e "\e[31m This script is expected to run with sudo or as a root user \e[0m   \n\t Ex:  bash scriptName compName"
     exit 1
 fi 
 
 echo "Intalling Nginx Web Server"
-dnf install nginx -y 
+dnf install nginx -y        &>>  /tmp/frontend.log
 
 echo "Enabling the service"
-systemctl enable nginx
+systemctl enable nginx      &>>  /tmp/frontend.log
 
 echo "Starting the Web Server"
-systemctl start nginx
+systemctl start nginx       &>>  /tmp/frontend.log

12919292e90191fcf4ee70c61305a48f4da73dd0
CodingManoj <mvp2612@hotmail.com>
2024-03-12 21:07:24 -0400

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,26 +8,24 @@ if [ $ID -ne 0 ] ; then
     exit 1
 fi 
 
-echo -n "Intalling Nginx Web Server"
-dnf install nginx -y        &>>  /tmp/frontend.log
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo "\e[31m Failure \e[0m"
-fi 
+LOGFILE="/tmp/frontend.log"
 
-echo -n "Enabling the service"
-systemctl enable nginx      &>>  /tmp/frontend.log
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo "\e[31m Failure \e[0m"
-fi 
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo "\e[31m Failure \e[0m"
+    fi 
+}
 
-echo -n "Starting the Web Server"
-systemctl start nginx       &>>  /tmp/frontend.log
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo "\e[31m Failure \e[0m"
-fi 
+echo -n "Intalling Nginx Web Server :"
+dnf install nginx -y        &>>  $LOGFILE
+stat $?
+
+echo -n "Enabling the service :"
+systemctl enable nginx      &>>  $LOGFILE
+stat $?
+
+echo -n "Starting the Web Server: "
+systemctl start nginx       &>>  $LOGFILE
+stat $?

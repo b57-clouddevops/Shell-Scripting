25a71df8a2f7ff8a50494655f542d86bfe52965d
CodingManoj <mvp2612@hotmail.com>
2024-03-12 21:30:24 -0400

    Bash Auto Push

---
+++ roboshop/components/mongodb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check whether you're running this script with sudo or a root user or not.If not, exit the script
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m This script is expected to run with sudo or as a root user \e[0m   \n\t Ex:  bash scriptName compName"
+    exit 1
+fi 
+
+COMPONENT="mongodb"
+LOGFILE="/tmp/$1.log"
+MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo"
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo "\e[31m Failure \e[0m"
+    fi 
+}
+
+curl -s -o /etc/yum.repos.d/mongodb.repo $MONGO_REPO
+stat $? 
+
+dnf install -y mongodb-org  &>>  $LOGFILE
+stat $? 
+
+echo -n "Enabling $COMPONENT Service :"
+systemctl enable mongod     &>>  $LOGFILE
+stat $?
+
+echo -n "Starting $COMPONENT Service :"
+systemctl start mongod     &>>  $LOGFILE
+stat $?

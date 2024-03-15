1c863f8e6ad5e1d29196408900ad1385981e5052
CodingManoj <CodingManoj@gmail.com>
2024-03-15 14:43:39 -0400

    Notes Update

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export mysql_root_password=$2
-
-if [ -z "$mysql_root_password" ]; then
-    echo "mysql_root_password is missing"
-    exit 8
-fi
+if [ "$1" = "mysql" ]
+    if [ -z "$mysql_root_password" ]; then
+        echo "mysql_root_password is missing"
+        exit 8
+    fi
+fi 
 
 echo "PPP printing $mysql_root_password PPP"
 bash components/$1.sh

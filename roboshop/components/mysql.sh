952b82f43d373f13826e2be97101ebd89b6519fb
CodingManoj <mvp2612@hotmail.com>
2024-03-14 21:40:03 -0400

    Bash Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -4,7 +4,6 @@ COMPONENT="mysql"
 LOGFILE="/tmp/${COMPONENT}.log"
 MYSQL_REPO="https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mysql.repo"
 SCHEMA_URL="https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/shipping.sql"
-MYSQL_PWD=${2}
 
 source components/common.sh         # source will keep all the functions local to the current script that declared in other file.
 
@@ -38,7 +37,7 @@ if [ $? -ne 0 ] ; then
     stat $? 
 fi 
 
-echo "show plugins;" | mysql -uroot -p${MYSQL_PWD}| grep validate_password &>>  $LOGFILE
+echo "show plugins;" | mysql -uroot -pRoboShop@1| grep validate_password &>>  $LOGFILE
 if [ $? -eq 0 ] ; then 
     echo -n "Uninstalling plugins:"
     echo "uninstall plugin validate_password;" | mysql -uroot -p${MYSQL_PWD}

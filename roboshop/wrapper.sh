6bdc3d75d21b151a8ef33d9aa0d21bf0046aa093
CodingManoj <CodingManoj@gmail.com>
2024-03-15 14:25:41 -0400

    Notes Update

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -2,12 +2,11 @@
 
 mysql_root_password=$2
 
-if [ $1 -eq mysql ]
-    if [ -z "$mysql_root_password" ]; then
-        echo "mysql_root_password is missing"
-        exit 8
-    fi
+if [ -z "$mysql_root_password" ]; then
+    echo "mysql_root_password is missing"
+    exit 8
 fi
+
 
 echo printing $mysql_root_password
 bash components/$1.sh

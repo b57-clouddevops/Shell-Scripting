8752f7c8633133e5779614938788de3b39328af3
CodingManoj <CodingManoj@gmail.com>
2024-03-14 23:27:23 -0400

    Notes Update

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -10,22 +10,22 @@ source components/common.sh         # source will keep all the functions local t
 
 echo -e "\e[35m ****** ______ $COMPONENT Configuration Is Started ****** ______ \e[0m"
 
-echo "Disabling $COMPONENT repo :"
-dnf module disable mysql -y     &>>  $LOGFILE
-stat $? 
+# echo "Disabling $COMPONENT repo :"
+# dnf module disable mysql -y     &>>  $LOGFILE
+# stat $? 
 
-echo -n "Configuring $COMPONENT repo"
-curl -s -L -o /etc/yum.repos.d/mysql.repo $MYSQL_REPO
-stat $? 
+# echo -n "Configuring $COMPONENT repo"
+# curl -s -L -o /etc/yum.repos.d/mysql.repo $MYSQL_REPO
+# stat $? 
 
-echo -n "Installing $COMPONENT :"
-dnf install mysql-community-server -y  &>>  $LOGFILE
-stat $? 
+# echo -n "Installing $COMPONENT :"
+# dnf install mysql-community-server -y  &>>  $LOGFILE
+# stat $? 
 
-echo -n "Starting $COMPONENT :"
-systemctl enable mysqld    &>>  $LOGFILE
-systemctl start mysqld     &>>  $LOGFILE
-stat $? 
+# echo -n "Starting $COMPONENT :"
+# systemctl enable mysqld    &>>  $LOGFILE
+# systemctl start mysqld     &>>  $LOGFILE
+# stat $? 
 
 echo -n "Fetching $COMPONENT root passowrd: "
 DEFAULT_ROOT_PASS=$(grep "temporary password" /var/log/mysqld.log | awk -F " " '{print $NF}')

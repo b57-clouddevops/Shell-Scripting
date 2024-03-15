0eba4f4d76bf9ccb3b7def8b4c95828f8c1ea178
CodingManoj <CodingManoj@gmail.com>
2024-03-15 14:51:38 -0400

    Notes Update

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export mysql_root_password=$2
+export mysql_root_password=$2                     # Export makes sure the variable saves in the system heap memory and then it can be used in other scripts called by it as well
 if [ "$1" = "mysql" ]; then
     if [ -z "$mysql_root_password" ]; then
         echo "mysql_root_password is missing"

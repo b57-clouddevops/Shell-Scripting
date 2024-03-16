762a2498af0cb15d52125b528910f92fd401564d
CodingManoj <mvp2612@hotmail.com>
2024-03-15 22:08:10 -0400

    Bash Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-# This script creates Ec2
+# This script creates EC2 Instaces & the associated DNS Records for the created servers
 
 AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-03b4d0f3339fdc89c"               # Create your own Security Group that allows allows all and then add your SGID 
@@ -12,5 +12,7 @@ if [ -z $1 ] ; then
     exit 1
 fi 
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]"
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .Instances[].PrivateIpAddress |sed -e 's/"//g')
+echo "$1 Server Created and here is the IP ADDRESS $PRIVATE_IP"
+
 

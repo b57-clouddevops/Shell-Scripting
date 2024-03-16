39237345e61e72d30368b515b60a267adfea043f
CodingManoj <mvp2612@hotmail.com>
2024-03-15 21:55:52 -0400

    Bash Auto Push

---
+++ roboshop/launch-ec2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+# This script creates Ec2
+
+AMI_ID="ami-072983368f2a6eab5"
+SGID="sg-03b4d0f3339fdc89c"               # Create your own Security Group that allows allows all and then add your SGID 
+COMPONENT=$1
+
+if [ -z $1 ] ; then
+    echo -e "\e[31m   COMPONENT NAME IS NEEDED: \e[0m"
+    echo -e "\e[36m \t\t Example Usage : \e[0m  bash launch-ec2 ratings"
+fi 
+
+aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]'
+

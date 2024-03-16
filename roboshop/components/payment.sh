659019bcc99f6f61438c1b31821b6a04f057d514
CodingManoj <mvp2612@hotmail.com>
2024-03-15 20:57:52 -0400

    Bash Auto Push

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+COMPONENT="payment"
+source components/common.sh         
+echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
+# CALLING NODEJS FUNCTION
+PYTHON 
+echo -e "\e[35m Configuring ${COMPONENT} IS Completed \e[0m \n"
